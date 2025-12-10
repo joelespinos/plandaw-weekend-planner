977077e898dada6b70f33177b818275d83071f12
JoelEspinos <jespinosruiz@gmail.com>
2025-12-10 01:50:55 +0100

    Header i Footer, estructura + estils
    S'ha estructurat els 2 components del layout y s'han estilat
    S'ha afegit llibreria Fontawesome

--- src/app/view/layout/footer/footer.ts
+++ src/app/view/layout/footer/footer.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal, Signal } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHeart as fasHeart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
+  // Icons
+  public _heartIcon: Signal<any>;
 
+  constructor() {
+    this._heartIcon = signal<any>(fasHeart).asReadonly();
+  }
+
+  public getCurrentYear() {
+    let dateObject = new Date();
+    return dateObject.getFullYear();
+  }
 }
--- src/app/view/layout/header/header.ts
+++ src/app/view/layout/header/header.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, signal, Signal } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHouse as fasHouse } from '@fortawesome/free-solid-svg-icons'
+import { faLightbulb as fasLightbulb } from '@fortawesome/free-solid-svg-icons';
+import { faUmbrellaBeach as fasUmbrellaBeach } from '@fortawesome/free-solid-svg-icons';
+import { faUserGroup as fasUserGroup } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterModule, FontAwesomeModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  // Icons
+  public _homeIcon: Signal<any>;
+  public _ideasIcon: Signal<any>;
+  public _weekendIcon: Signal<any>;
+  public _friendsIcon: Signal<any>;
 
+  constructor() {
+    this._homeIcon = signal<any>(fasHouse).asReadonly();
+    this._ideasIcon = signal<any>(fasLightbulb).asReadonly();
+    this._weekendIcon = signal<any>(fasUmbrellaBeach).asReadonly();
+    this._friendsIcon = signal<any>(fasUserGroup).asReadonly();
+  }
 }
